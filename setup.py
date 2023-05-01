69960a40c864e8917be5e719a9801e3c781b98fa
Swen Gross <swen_g@t-online.de>
2023-05-01 17:22:52 +0200

    version automation (#13)

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import os
 
 from setuptools import setup
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
 
 def package_files(directory):
     paths = []
@@ -10,13 +12,46 @@ def package_files(directory):
             paths.append(os.path.join('..', path, filename))
     return paths
 
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_cli_client', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
 
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+    
 setup(
     name='ovos-cli-client',
-    version="0.0.1",
+    version=get_version(),
     packages=['ovos_cli_client'],
-    url='https://github.com/OpenVoiceOS/ovos_cli_client',
-    install_requires=["ovos_utils>=0.0.31a6", "ovos-bus-client~=0.0,>=0.0.3a4"],
+    url='https://github.com/OpenVoiceOS/ovos-cli-client',
+    install_requires=required("requirements/requirements.txt"),
     package_data={'': package_files('ovos_cli_client')},
     include_package_data=True,
     license='Apache',
